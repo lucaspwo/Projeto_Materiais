bd30ba1b267ce35aeac6c2373e5246d31d02dfe2
Lucas Oliveira <lucaspwo@gmail.com>
2018-05-19 22:16:08 -0300

    Atual. 19/05/18

--- stepper_oneRevolution/stepper_oneRevolution.ino
+++ stepper_oneRevolution/stepper_oneRevolution.ino
@@ -20,31 +20,39 @@
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
 // for your motor
 
-const int turns = 4;
-const int wait = 2000; //in seconds
+bool flag = false;
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+Stepper myStepper(stepsPerRevolution, 1, 2, 3, 4);
 
 void setup() {
-  pinMode(7, INPUT);
+  pinMode(0, INPUT);
   // set the speed at 60 rpm:
   myStepper.setSpeed(120);
-  // initialize the serial port:
-  Serial.begin(9600);
 }
 
 void loop() {
-  if(digitalRead(7) == true){
-  // step one revolution  in one direction:
-//    Serial.println("counterclockwise");
-    myStepper.step(-1);
+  if(digitalRead(0) == true){
+    flag = true;
   }
-//  delay(2000);
-
-  // step one revolution in the other direction:
-//  Serial.println("counterclockwise");
-//  myStepper.step(-stepsPerRevolution);
-//  delay(500);
+  else if(digitalRead(0) == false){
+    flag = false;
+    digitalWrite(1,LOW);
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+  }
+  while(flag == true){
+    // step one revolution  in one direction:
+    myStepper.step(-1);
+    if(digitalRead(0) == false){
+      flag = false;
+      digitalWrite(1,LOW);
+      digitalWrite(2,LOW);
+      digitalWrite(3,LOW);
+      digitalWrite(4,LOW);
+      break;
+    }
+  }
 }
 
