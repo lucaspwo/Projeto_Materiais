d2cb212320410ce2ea441ae30691411d92b6d520
Lucas Oliveira <lucaspwo@gmail.com>
2018-05-31 02:07:23 -0300

    Peq. Modificações

--- stepper_oneRevolution/stepper_oneRevolution.ino
+++ stepper_oneRevolution/stepper_oneRevolution.ino
@@ -16,6 +16,11 @@
  */
 
 #include <Stepper.h>
+#define BTN 0
+#define IN1 1
+#define IN2 2
+#define IN3 3
+#define IN4 4
 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
 // for your motor
@@ -23,37 +28,38 @@ const int stepsPerRevolution = 200;  // change this to fit the number of steps p
 bool flag = false;
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 1, 2, 3, 4);
+Stepper myStepper(stepsPerRevolution, IN1, IN2, IN3, IN4);
 
 void setup() {
-  pinMode(0, INPUT);
+  pinMode(BTN, INPUT);
   // set the speed at 60 rpm:
   myStepper.setSpeed(120);
 }
 
+void stepperOFF(){
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+}
+
 void loop() {
-  if(digitalRead(0) == true){
-//    flag = true;
-    myStepper.step(1);
+  if(digitalRead(BTN) == true){
+    flag = true;
+//    myStepper.step(1);
   }
-  else if(digitalRead(0) == false){
-//    flag = false;
-    digitalWrite(1,LOW);
-    digitalWrite(2,LOW);
-    digitalWrite(3,LOW);
-    digitalWrite(4,LOW);
+  else if(digitalRead(BTN) == false){
+    flag = false;
+    stepperOFF();
   }
-//  while(flag == true){
-//    // step one revolution  in one direction:
-//    myStepper.step(-1);
-//    if(digitalRead(0) == false){
-//      flag = false;
-//      digitalWrite(1,LOW);
-//      digitalWrite(2,LOW);
-//      digitalWrite(3,LOW);
-//      digitalWrite(4,LOW);
-//      break;
-//    }
-//  }
+  while(flag == true){
+    // step one revolution  in one direction:
+    myStepper.step(-1);
+    if(digitalRead(BTN) == false){
+      flag = false;
+      stepperOFF();
+      break;
+    }
+  }
 }
 
